5db7512731088d6c72159e62ba9c04c147a33a9f
Arti Kumari <34932809+artik05@users.noreply.github.com>
2022-07-08 14:06:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1  data-ns-test="project-name">This is my first React project</h1>
+      <p data-ns-test="project-description">Start editing to see some magic happen!</p>
+    </div>
+  );
+}
